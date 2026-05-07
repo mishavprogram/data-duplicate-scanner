745a5314b2b18f71dc06f1e9038d9a974eed7f0f
Mykhailo Vinnichuk <mishavprogram@ukr.net>
2026-05-07 09:38:49 +0300

    chore: initialize Spring Boot project

---
+++ src/main/java/com/mykhailo/dataduplicatescanner/DataDuplicateScannerApplication.java
@@ -0,0 +1,13 @@
+package com.mykhailo.dataduplicatescanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataDuplicateScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataDuplicateScannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mykhailo/dataduplicatescanner/DataDuplicateScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mykhailo.dataduplicatescanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataDuplicateScannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
